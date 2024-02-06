e1846690a24dff1a255cb574bedfc7de75eb4b8b
pablogarcia01 <pablogarciaser14@gmail.com>
2024-02-06 14:32:20 +0100

    Agregar método mostrarFechaYHora

--- Principal.java
+++ Principal.java
@@ -1,6 +1,17 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        mostrarFechaYHora();
+    }
+
+    public static void mostrarFechaYHora() {
+        LocalDateTime fechaHoraActual = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = fechaHoraActual.format(formatter);
+        System.out.println("Fecha y hora actual: " + formattedDate);
     }
 }
 
